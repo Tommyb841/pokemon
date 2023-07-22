adab3ba45fc66f327c36ace836e9a36ee0fccd74
tommyb841 <thomasb841@yahoo.com>
2023-07-21 23:08:38 -0400

    added pokemon compponent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import Pokedex from '../components/Pokedex'
 export default function index() {
   return (
+    <>
     <div>Hello world</div>
+    <Pokedex/>
+    </>
+
   )
 }
