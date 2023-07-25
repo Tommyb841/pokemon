2e6f73077f98021547e25c163125dc6dda713fb2
tommyb841 <thomasb841@yahoo.com>
2023-07-25 09:56:52 -0400

    added picture and made list 151

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import Link from "gatsby-link";
+import PokemonPics from "./PokemonPics";
 export default function PokemonList({ pokemon }) {
   return (
-    <div>
-      {pokemon.map((p) => (
-        <div key={p}>{p}</div>
+    <>
+      {pokemon.map((p, idx) => (
+        <ul>
+          <button key={idx}>{p}</button>
+          <PokemonPics id={idx + 1} />
+        </ul>
       ))}
-    </div>
+      ;
+    </>
   );
 }
