2d0f3a16b0841630aaabdeefc1dfd6d0ef4c1049
tommyb841 <thomasb841@yahoo.com>
2023-07-25 09:56:22 -0400

    created pokemon pics

---
+++ src/components/PokemonPics.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+export default function PokemonPics({ id }) {
+  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+  return <img style={{ width: "50px", height: "50px" }} src={url} alt="" />;
+}
