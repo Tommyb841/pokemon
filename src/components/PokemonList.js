3f530b35fd90385d6cb372e212d46ee98ff4af8d
tommyb841 <thomasb841@yahoo.com>
2023-07-26 14:17:16 -0400

    adding link to pokeinfo need context to pass name

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import PokemonPics from "./PokemonPics";
+import PokemonInfo from "./PokemonInfo";
+import { Link } from "gatsby";
+import { PokeContext } from "./PokeContext";
+
 export default function PokemonList({ pokemon }) {
+  const { pokeName, setPokeName } = useContext(PokeContext);
   return (
     <>
       {pokemon.map((p, idx) => (
-        <ul>
-          <PokemonPics id={idx + 1} /><br/>
-          <button key={idx}>{p}</button>
-        </ul>
+        <div>
+          <ul>
+            <PokemonPics id={idx + 1} />
+            <br />
+            <PokemonInfo />
+            {/* <Link to={`pokeinfo/${p}`}>
+              <button key={idx + 1}>{p}</button>
+            </Link> */}
+          </ul>
+        </div>
       ))}
       ;
     </>
