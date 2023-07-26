4d135f23f7567d4e710fb3de8ebfa3b3cc3a0ea5
tommyb841 <thomasb841@yahoo.com>
2023-07-26 14:18:20 -0400

    need to get name passed from pokelist

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,5 +1,14 @@
-import React from "react";
-
-export default function PokemonInfo() {
-  return <div>PokemonInfo</div>;
+import React, { useContext } from "react";
+import Pokemon from "../components/Pokemon";
+import { Link } from "gatsby";
+import { PokeContext } from "./PokeContext";
+export default function PokemonInfo({ params }) {
+  const { pokeName, setPokeName } = useContext(PokeContext);
+  return (
+    <>
+      <Link to={`pokeinfo`}>
+        <button>info</button>
+      </Link>
+    </>
+  );
 }
