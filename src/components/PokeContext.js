a7627fd54e47017a9df6032be1a2612d4cecdbd9
tommyb841 <thomasb841@yahoo.com>
2023-07-26 21:50:37 -0400

    may not need

--- src/components/PokeContext.js
+++ src/components/PokeContext.js
@@ -1,14 +1,14 @@
-import React, { createContext, useState } from "react";
+// import React, { createContext, useState } from "react";
 
-const PokeContext = createContext();
-const PokeContextProvider = ({ children }) => {
-  const [pokeName, setPokeName] = useState("");
+// const PokeContext = createContext();
+// const PokeContextProvider = ({ children }) => {
+//   const [pokeName, setPokeName] = useState("");
 
-  return (
-    <PokeContext.Provider value={{ pokeName, setPokeName }}>
-      {children}
-    </PokeContext.Provider>
-  );
-};
+//   return (
+//     <PokeContext.Provider value={{ pokeName, setPokeName }}>
+//       {children}
+//     </PokeContext.Provider>
+//   );
+// };
 
-export { PokeContext, PokeContextProvider };
+// export { PokeContext, PokeContextProvider };
