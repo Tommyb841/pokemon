1e17996168666ba742eb802e0c39ca3229817be4
tommyb841 <thomasb841@yahoo.com>
2023-07-28 19:50:05 -0400

    got props info , needs data from api

--- src/pages/pokeinfo.js
+++ src/pages/pokeinfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import PokemonInfo from "../components/PokemonInfo";
 
 export default function PokeInfo(props) {
+  console.log("props from pokeinfo: ", props);
   return (
     <>
       <h1>this is the info page</h1>
