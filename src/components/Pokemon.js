5a8a8a712ee02c008615581a36e21270bbfcfba5
tommyb841 <thomasb841@yahoo.com>
2023-07-28 19:54:04 -0400

    source of pokemon pic and link to info

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,52 +1,14 @@
 import React, { useEffect, useState } from "react";
-import PokemonList from "./PokemonList";
-import axios from "axios";
-//import Pagination from "./Pagination";
-export default function Pokemon() {
-  
-  const [pokemon, setPokemon] = useState([]);
-  //const [pokeIndex, setPokeindex] = useState([]);
-  const [currentPageUrl, setCurrentPageUrl] = useState(
-    `https://pokeapi.co/api/v2/pokemon?limit=151`
-  );
-  // const [nextPageUrl, setNextPageUrl] = useState();
-  // const [prevPageUrl, setPrevPageUrl] = useState();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    let cancel;
-    axios
-      .get(currentPageUrl, {
-        cancelToken: new axios.CancelToken((c) => (cancel = c)),
-      })
-      .then((res) => {
-        setLoading(false);
-        // setNextPageUrl(res.data.next);
-        // setPrevPageUrl(res.data.previous);
-        setPokemon(res.data.results.map((p) => p.name));
-      });
-    return () => cancel();
-  }, [currentPageUrl]);
-  if (loading) return "Loading... ";
-
-  // function gotoNextPage() {
-  //   setCurrentPageUrl(nextPageUrl);
-  // }
-
-  // function gotoPrevPage() {
-  //   setCurrentPageUrl(prevPageUrl);
-  // }
-
+import { Link } from "gatsby";
+export default function Pokemon({ pokemon }) {
+  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+  console.log("Pokemon inside poke component: ", pokemon);
   return (
-    <>
-      <div>
-        <PokemonList pokemon={pokemon} />
-        {/* <Pagination
-          gotoNextPage={nextPageUrl ? gotoNextPage : null}
-          gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
-        /> */}
-      </div>
-    </>
+    <div>
+      <img style={{ width: "50px", height: "50px" }} src={url} alt="" />
+      <Link to={`/pokeinfo`} state={pokemon}>
+        <button>{pokemon.name}</button>
+      </Link>
+    </div>
   );
 }
