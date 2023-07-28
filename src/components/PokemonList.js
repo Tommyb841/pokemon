d7f8e1f06456e45b23bcc56ad0836516fdf0023b
tommyb841 <thomasb841@yahoo.com>
2023-07-28 18:04:27 -0400

    added css (not working)

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,23 +1,26 @@
 import React from "react";
 import PokemonPics from "./PokemonPics";
 import PokemonInfo from "./PokemonInfo";
+import styles from "../styles/pokelist.module.css";
 
 export default function PokemonList({ pokemon }) {
   return (
     <>
-      {pokemon.map((p, idx) => (
-        <div>
-          <ul>
-            <PokemonPics id={idx + 1} />
-            <br />
-            <PokemonInfo pokemon={[p]} />
-            {/* <Link to={`pokeinfo/${p}`}>
+      <div className={styles.p}>
+        {pokemon.map((p, idx) => (
+          <div>
+            <ul>
+              <PokemonPics id={idx + 1} />
+              <br />
+              <PokemonInfo pokemon={[p]} />
+              {/* <Link to={`pokeinfo/${p}`}>
               <button key={idx + 1}>{p}</button>
             </Link> */}
-          </ul>
-        </div>
-      ))}
-      ;
+            </ul>
+          </div>
+        ))}
+        ;
+      </div>
     </>
   );
 }
