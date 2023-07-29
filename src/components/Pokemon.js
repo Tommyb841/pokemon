56e731805c608129f5307192e1dbdc4810d42e37
tommyb841 <thomasb841@yahoo.com>
2023-07-29 16:02:08 -0400

    added styles sheet

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,14 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "gatsby";
+// import React from "react"
+// import { Link } from "gatsby"
+// export default function Pokemon({ pokemon }) {
+//   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+//   console.log("Pokemon inside poke component: ", pokemon)
+//   return (
+//     <div>
+//       <img style={{ width: "50px", height: "50px" }} src={url} alt="" />
+//       <Link to={`/pokeinfo`} state={pokemon}>
+//         <button>{pokemon.name}</button>
+//       </Link>
+//     </div>
+//   )
+// }
+import React from "react"
+import { Link } from "gatsby"
+import "../styles/pokemon.css" // Import the CSS file
+
 export default function Pokemon({ pokemon }) {
-  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
-  console.log("Pokemon inside poke component: ", pokemon);
+  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+  console.log("Pokemon inside poke component: ", pokemon)
   return (
-    <div>
-      <img style={{ width: "50px", height: "50px" }} src={url} alt="" />
+    <div className="pokemon-container">
+      <img src={url} alt={pokemon.name} />
       <Link to={`/pokeinfo`} state={pokemon}>
         <button>{pokemon.name}</button>
       </Link>
     </div>
-  );
+  )
 }
