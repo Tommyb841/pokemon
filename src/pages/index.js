36d9b95947831f51407b7a1309d352d0a9f28b59
tommyb841 <thomasb841@yahoo.com>
2023-07-29 16:04:43 -0400

    removed semi colons lol

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import PokemonList from "../components/PokemonList";
-import axios from "axios";
-const pageUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`;
+import React, { useEffect, useState } from "react"
+import PokemonList from "../components/PokemonList"
+import axios from "axios"
+const pageUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`
 export default function Index() {
-  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonList, setPokemonList] = useState([])
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setLoading(true);
-    let cancel;
+    setLoading(true)
+    let cancel
     axios
       .get(pageUrl, {
         cancelToken: new axios.CancelToken((c) => (cancel = c)),
       })
       .then((res) => {
-        setLoading(false);
+        setLoading(false)
         setPokemonList(
           res.data.results.map((p, idx) => {
-            console.log("current pokemon: ", p);
+            console.log("current pokemon: ", p)
             let pokemon = {
               id: idx + 1,
               name: p.name,
-            };
-            return pokemon;
-          })
-        );
-      });
-    return () => cancel();
-  }, []);
-  if (loading) return "Loading... ";
+            }
+            return pokemon
+          }),
+        )
+      })
+    return () => cancel()
+  }, [])
+  if (loading) return "Loading... "
 
-  return <PokemonList pokemonList={pokemonList} />;
+  return <PokemonList pokemonList={pokemonList} />
 }
