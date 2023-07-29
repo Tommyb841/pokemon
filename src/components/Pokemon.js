0c48244d85b3860dc2bd3170fa5f1c4e2bcc5889
tommyb841 <thomasb841@yahoo.com>
2023-07-29 16:02:42 -0400

    added style sheet

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,17 +1,3 @@
-// import React from "react"
-// import { Link } from "gatsby"
-// export default function Pokemon({ pokemon }) {
-//   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
-//   console.log("Pokemon inside poke component: ", pokemon)
-//   return (
-//     <div>
-//       <img style={{ width: "50px", height: "50px" }} src={url} alt="" />
-//       <Link to={`/pokeinfo`} state={pokemon}>
-//         <button>{pokemon.name}</button>
-//       </Link>
-//     </div>
-//   )
-// }
 import React from "react"
 import { Link } from "gatsby"
 import "../styles/pokemon.css" // Import the CSS file
