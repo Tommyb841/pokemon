df085f07251f895e2cd4cfb68c80d91bf7eccb0f
tommyb841 <thomasb841@yahoo.com>
2023-07-30 13:27:10 -0400

    deleted

--- src/components/PokemonPics.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect } from "react";
-
-export default function PokemonPics({ id }) {
-  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-  return <img style={{ width: "50px", height: "50px" }} src={url} alt="" />;
-}
